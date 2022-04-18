3a02c83f18e196736560918fc05065a0b71810c3
Josh Illichmann <josh@studioprisoner.com>
2022-04-18 17:50:14 +1000

    fixes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const { withContentlayer } = require('next-contentlayer');
+
+/**
+ * @type {import('next').NextConfig}
+ */
+module.exports = withContentlayer()({
+  swcMinify: true,
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      'i.scdn.co', // Spotify Album Art
+      'pbs.twimg.com' // Twitter Profile Picture
+    ]
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat'
+      });
+    }
 
-const { withContentlayer } = require('next-contentlayer') 
-
-module.exports = withContentlayer
+    return config;
+  }
+});
