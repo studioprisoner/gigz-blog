e1f2c6af42d728994e96f53fb31bce1ee88890d3
Josh Illichmann <josh@studioprisoner.com>
2022-04-18 17:14:32 +1000

    fixing subscribe tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 
 import Container from '../components/Container'
+import Subscribe from '../components/Subscribe'
 
 export default function Home() {
   return (
@@ -12,28 +13,7 @@ export default function Home() {
                 <span className="block xl:inline">Gigz App</span>{' '}
                 <span className="block xl:inline text-pink-500">Coming Soon</span>
               </h1>
-              <p className="text-white text-base leading-relaxed mt-8">Gigz is a concert companion that helps you store your memories. Now you won’t be able to forget the amazing moments you've experiencd, just tap the app to make them stick!</p>
-              <p className="text-white text-base leading-relaxed mt-8">Coming soon, sign up to join the waiting list.</p>
-              <form className="sm:flex pt-8"
-              action="https://buttondown.email/api/emails/embed-subscribe/studioprisoner"
-              method="post"
-              target="_self"
-              onSubmit="window.open('https://buttondown.email/studioprisoner', 'popupwindow')"
-              >
-              <label htmlFor="email-address" className="sr-only">
-                  Email address
-                </label>
-                <input
-                  id="email-address"
-                  name="email-address"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="w-full border-white px-5 py-3 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-700 focus:ring-white rounded-md"
-                  placeholder="Enter your email"
-                />
-                <button className="mt-3 w-full flex items-center justify-center px-5 py-3 border border-transparent shadow text-base font-medium rounded-md text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-pink-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-pink-500 focus:ring-white sm:mt-0 sm:ml-3 sm:w-auto sm:flex-shrink-0" type="submit" value="Subscribe">Notify Me</button>
-              </form>
+              <Subscribe />
             </div>
             <div className="col-span-6 flex justify-center">
               <img
