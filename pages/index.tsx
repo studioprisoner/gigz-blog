b1a6eef5a9b3681a7534ebce76789fdff553866f
Josh Illichmann <josh@studioprisoner.com>
2022-04-25 18:56:50 +1000

    fixed colspan

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className='py-16'>
         <div className="container px-4 sm:px-8 lg:px-16 xl:px-20 mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-12 gap-8 items-center">
-            <div className="col-span-6">
+            <div className="col-span-8">
               <h1 className="font-bold font-epilogue text-4xl md:text-5xl max-x-xl text-gray-900 dark:text-white leading-light">
                 <span className="block xl:inline">Gigz App</span>{' '}
                 <span className="block xl:inline text-pink-500">Coming Soon</span>
