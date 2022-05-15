09b5f613c8b7090e662c0f8700c54f26a752a7e1
Josh Illichmann <josh@studioprisoner.com>
2022-05-16 06:24:40 +1000

    removed dark mode for devices with default system

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,10 @@ const { spacing, fontFamily } = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: ['./pages/**/*.tsx', './components/**/*.tsx', './layouts/**/*.tsx'],
-  darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
-        'speaker-pattern': "url('/assets/img/speaker-pattern.png')",
-        'hero-background-light': "url('/assets/img/bg-light.webp')"
+        'speaker-pattern': "url('/assets/img/speaker-pattern.png')"
       },
       colors: {
         'gigz-purple': '#852DF5',
@@ -22,11 +20,11 @@ module.exports = {
       typography: (theme) => ({
         DEFAULT: {
           css: {
-            color: theme('colors.gray.700'),
+            color: theme('colors.slate.800'),
             a: {
-              color: theme('colors.blue.500'),
+              color: theme('colors.gigz.purple'),
               '&:hover': {
-                color: theme('colors.blue.700')
+                color: theme('colors.gigz.pink')
               },
               code: { color: theme('colors.blue.400') }
             },
@@ -40,55 +38,9 @@ module.exports = {
             'blockquote p:first-of-type::before': false,
             'blockquote p:last-of-type::after': false
           }
-        },
-        dark: {
-          css: {
-            color: theme('colors.gray.200'),
-            a: {
-              color: theme('colors.blue.400'),
-              '&:hover': {
-                color: theme('colors.blue.600')
-              },
-              code: { color: theme('colors.blue.400') }
-            },
-            blockquote: {
-              borderLeftColor: theme('colors.gray.700'),
-              color: theme('colors.gray.300')
-            },
-            'h2,h3,h4': {
-              color: theme('colors.gray.100'),
-              'scroll-margin-top': spacing[32]
-            },
-            hr: { borderColor: theme('colors.gray.700') },
-            ol: {
-              li: {
-                '&:before': { color: theme('colors.gray.500') }
-              }
-            },
-            ul: {
-              li: {
-                '&:before': { backgroundColor: theme('colors.gray.500') }
-              }
-            },
-            strong: { color: theme('colors.gray.100') },
-            thead: {
-              th: {
-                color: theme('colors.gray.100')
-              },
-              borderBottomColor: theme('colors.gray.600')
-            },
-            tbody: {
-              tr: {
-                borderBottomColor: theme('colors.gray.700')
-              }
-            }
-          }
         }
       })
     }
-  },
-  variants: {
-    typography: ['dark']
   },
   plugins: [require('@tailwindcss/typography')]
 };
