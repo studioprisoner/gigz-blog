c71ba150023352988d6bc894802b94d1044407f3
Josh Illichmann <josh@studioprisoner.com>
2022-05-16 23:13:56 +1000

    remove middleware

--- lib/posthog-node.tsx
+++ lib/posthog-node.tsx
@@ -39,7 +39,7 @@ export async function getFeatureFlagVariant(
   console.log('getFeatureFlagVariant', distinctUserId, featureName)
 
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_POSTHOG_HOST}`,
+    `${process.env.NEXT_PUBLIC_POSTHOG_HOST}/decide?v=2`,
     {
       method: 'POST',
       body: JSON.stringify({
