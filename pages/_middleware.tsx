a12fa97394529916fa2b5c1c31c85d80ec7af85a
Josh Illichmann <josh@studioprisoner.com>
2022-05-17 21:35:21 +1000

    trying new API

---
+++ pages/_middleware.tsx
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { isFeatureFlagEnabled } from '@lib/posthog-node'
+import { DISTINCT_ID_COOKIE_NAME, FEATURE_FLAGS } from '@lib/constants'
+
+export async function middleware(req: NextRequest) {
+  const url = req.nextUrl.clone()
+
+  // Redirect paths that go directly to the variant
+  if (url.pathname != '/') {
+    url.pathname = '/new_homepage'
+    return NextResponse.redirect(url)
+  }
+
+  const newHomePageFlagEnabled = await isFeatureFlagEnabled(
+    req.cookies[DISTINCT_ID_COOKIE_NAME],
+    FEATURE_FLAGS.NEW_INDEX_PAGE
+  )
+  url.pathname = newHomePageFlagEnabled ? '/new_homepage' : '/'
+
+  return NextResponse.rewrite(url)
+}
