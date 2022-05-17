7d251f8d49f88f2ac64a5cecc21948fdfc7b54ca
Josh Illichmann <josh@studioprisoner.com>
2022-05-17 22:33:10 +1000

    x

--- lib/constants.tsx
+++ lib/constants.tsx
@@ -2,7 +2,7 @@
  * List of known active Feature Flags
  */
  export const FEATURE_FLAGS = {
-  NEW_HOME_PAGE: 'New_Home_Page',
+  NEW_INDEX_PAGE: 'New_Home_Page',
 } as const
 
 export type FEATURE_FLAGS = typeof FEATURE_FLAGS[keyof typeof FEATURE_FLAGS]
