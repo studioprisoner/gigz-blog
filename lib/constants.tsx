6ceba07fbbdbff07c163ace154c78a410ab1a901
Josh Illichmann <josh@studioprisoner.com>
2022-05-17 22:13:03 +1000

    rename constant

--- lib/constants.tsx
+++ lib/constants.tsx
@@ -2,7 +2,7 @@
  * List of known active Feature Flags
  */
  export const FEATURE_FLAGS = {
-    NEW_INDEX_PAGE: 'New_Index_Page',
+    NEW_INDEX_PAGE: 'abtesthome',
   } as const
   
   export type FEATURE_FLAGS = typeof FEATURE_FLAGS[keyof typeof FEATURE_FLAGS]
